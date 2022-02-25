b9366172005c453d96d9c79af86e7205c33b0534
sjc-bui <bui@setjapan.co.jp>
2022-02-25 10:28:06 +0900

    init

---
+++ app/Models/Post.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $table = 'posts';
+
+    protected $fillable = ['user_id', 'title', 'body'];
+
+    /** Relations */
+    // One to many inverse relationship with User model
+    public function owner() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    // One to many relationship with Comment model
+    public function comments() {
+        return $this->hasMany(Comment::class, 'post_id');
+    }
+
+    /**
+     * show post route
+     * 
+     * @return string
+     */
+    public function path() {
+        return "/posts/{$this->id}";
+    }
+}
